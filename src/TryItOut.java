1038852f058e3b18d63c7ae71c261261ff565dc1
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2021-01-29 09:56:54 -0800

    Initial commit

---
+++ src/TryItOut.java
@@ -0,0 +1,8 @@
+public class TryItOut {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        double c = (double)a/(double)b;
+        System.out.println(c);
+    }
+}
